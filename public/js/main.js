1f05b3ab2f7ae9b48ecaabc983bc1ac86a503dad
李乾坤 <542154968@qq.com>
2018-11-01 14:38:07 +0800

    压缩和设置cdn

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             } else {
                 $('#header').removeClass('fixed')
             }
-        }, 200)
+        }, 100)
     })
     $(window).trigger('scroll')
 
